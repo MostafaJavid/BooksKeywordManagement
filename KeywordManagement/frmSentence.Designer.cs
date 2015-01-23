7ddd6659b79420b7ea96c2bc56cb2d62a2488703
Naser Safari <nassersafari@gmail.com>
2015-01-23 19:25:56 +0330

    fix (sentence)

---
+++ KeywordManagement/frmSentence.Designer.cs
@@ -0,0 +1,196 @@
+﻿using KeywordManagement.Domain;
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+namespace KeywordManagement
+{
+    public partial class frmSentence : Form
+    {
+        private Keyword keyword;
+        private FormMode formMode;
+        private Sentence sentence;
+        private IContainer components;
+        private Label label1;
+        private TextBox txtSentence;
+        private Label label2;
+        private Label label3;
+        private TextBox txtPageNo;
+        private Label label4;
+        private Label label5;
+        private TextBox txtDescription;
+        private Button bntSave;
+        private Button btnCancle;
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && this.components != null)
+            {
+                this.components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSentence = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPageNo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.bntSave = new System.Windows.Forms.Button();
+            this.btnCancle = new System.Windows.Forms.Button();
+            this.cbBookTitle = new System.Windows.Forms.ComboBox();
+            this.cbAuthor = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(555, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "جمله:";
+            // 
+            // txtSentence
+            // 
+            this.txtSentence.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSentence.Location = new System.Drawing.Point(12, 6);
+            this.txtSentence.Name = "txtSentence";
+            this.txtSentence.Size = new System.Drawing.Size(542, 20);
+            this.txtSentence.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(537, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "نام کتاب:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(201, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "نام نویسنده:";
+            // 
+            // txtPageNo
+            // 
+            this.txtPageNo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPageNo.Location = new System.Drawing.Point(455, 72);
+            this.txtPageNo.Name = "txtPageNo";
+            this.txtPageNo.Size = new System.Drawing.Size(60, 20);
+            this.txtPageNo.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(519, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "شماره صفحه:";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(534, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "توضیحات:";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(12, 118);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(574, 198);
+            this.txtDescription.TabIndex = 5;
+            // 
+            // bntSave
+            // 
+            this.bntSave.Location = new System.Drawing.Point(309, 332);
+            this.bntSave.Name = "bntSave";
+            this.bntSave.Size = new System.Drawing.Size(75, 23);
+            this.bntSave.TabIndex = 6;
+            this.bntSave.Text = "ثبت";
+            this.bntSave.UseVisualStyleBackColor = true;
+            this.bntSave.Click += new System.EventHandler(this.bntSave_Click);
+            // 
+            // btnCancle
+            // 
+            this.btnCancle.Location = new System.Drawing.Point(217, 332);
+            this.btnCancle.Name = "btnCancle";
+            this.btnCancle.Size = new System.Drawing.Size(75, 23);
+            this.btnCancle.TabIndex = 7;
+            this.btnCancle.Text = "انصراف";
+            this.btnCancle.UseVisualStyleBackColor = true;
+            this.btnCancle.Click += new System.EventHandler(this.btnCancle_Click);
+            // 
+            // cbBookTitle
+            // 
+            this.cbBookTitle.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.cbBookTitle.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.cbBookTitle.FormattingEnabled = true;
+            this.cbBookTitle.Location = new System.Drawing.Point(271, 37);
+            this.cbBookTitle.Name = "cbBookTitle";
+            this.cbBookTitle.Size = new System.Drawing.Size(260, 21);
+            this.cbBookTitle.TabIndex = 2;
+            // 
+            // cbAuthor
+            // 
+            this.cbAuthor.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.cbAuthor.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.cbAuthor.FormattingEnabled = true;
+            this.cbAuthor.Location = new System.Drawing.Point(12, 36);
+            this.cbAuthor.Name = "cbAuthor";
+            this.cbAuthor.Size = new System.Drawing.Size(183, 21);
+            this.cbAuthor.TabIndex = 3;
+            // 
+            // frmSentence
+            // 
+            this.ClientSize = new System.Drawing.Size(598, 367);
+            this.Controls.Add(this.cbAuthor);
+            this.Controls.Add(this.cbBookTitle);
+            this.Controls.Add(this.btnCancle);
+            this.Controls.Add(this.bntSave);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtPageNo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSentence);
+            this.Controls.Add(this.label1);
+            this.Name = "frmSentence";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "frmSentence";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private ComboBox cbBookTitle;
+        private ComboBox cbAuthor;
+    }
+}
---
+++ KeywordManagement/frmSentence.cs
@@ -0,0 +1,129 @@
+﻿using KeywordManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KeywordManagement
+{
+    public partial class frmSentence: Form
+    {
+        public frmSentence(FormMode formMode)
+        {
+            this.InitializeComponent();
+            this.formMode = formMode;
+            using (KeywordManagementContext keywordManagementContext = new KeywordManagementContext()){
+                cbBookTitle.AutoCompleteCustomSource.AddRange(keywordManagementContext.Books.Select(book => book.Title).ToArray());
+                cbAuthor.AutoCompleteCustomSource.AddRange(keywordManagementContext.Authors.Select(author => author.FullName).ToArray());
+            }
+            
+        }
+        public frmSentence(Keyword keyword, FormMode formMode)
+            : this(formMode)
+        {
+            this.keyword = keyword;
+        }
+        public frmSentence(Sentence sentence, FormMode formMode)
+            : this(formMode)
+        {
+            this.sentence = sentence;
+            this.txtSentence.Text = sentence.Content;
+            this.txtSentence.Enabled = false;
+        }
+        private void bntSave_Click(object sender, EventArgs e)
+        {
+            var bookTitile = cbBookTitle.Text;
+            var authorName = cbAuthor.Text;
+            if (String.IsNullOrEmpty(txtSentence.Text) || String.IsNullOrEmpty(bookTitile))
+                return;
+            Utils.DoWithWait(this, delegate
+            {
+                using (KeywordManagementContext keywordManagementContext = new KeywordManagementContext())
+                {
+                    switch (this.formMode)
+                    {
+                        case FormMode.Create:
+                            {
+                                keywordManagementContext.Keywords.Attach(this.keyword);
+                                Book theBook = keywordManagementContext.Books.FirstOrDefault((Book book) => book.Title == bookTitile);
+                                if (theBook == null)
+                                {
+                                    Author theAuthor;
+                                    if ((theAuthor = keywordManagementContext.Authors.FirstOrDefault((Author author) => author.FullName == authorName)) == null)
+                                    {
+                                        theAuthor = new Author
+                                        {
+                                            FullName = authorName
+                                        };
+                                    }
+                                    theBook = new Book
+                                    {
+                                        Title = bookTitile
+                                    };
+                                    theAuthor.Books.Add(theBook);
+                                    theBook.Authors.Add(theAuthor);
+                                }
+                                Sentence sentence = new Sentence
+                                {
+                                    Content = this.txtSentence.Text
+                                };
+                                Reference item = new Reference
+                                {
+                                    Description = this.txtDescription.Text,
+                                    PageNumber = this.txtPageNo.Text,
+                                    Sentence = sentence,
+                                    Book = theBook
+                                };
+                                sentence.References.Add(item);
+                                this.keyword.Sentences.Add(sentence);
+                                keywordManagementContext.SaveChanges();
+                                break;
+                            }
+                        case FormMode.Update:
+                            {
+                                keywordManagementContext.Sentences.Attach(this.sentence);
+                                Book theBook = keywordManagementContext.Books.FirstOrDefault((Book book) => book.Title == bookTitile);
+                                if (theBook == null)
+                                {
+                                    Author theAuthor;
+                                    if ((theAuthor = keywordManagementContext.Authors.FirstOrDefault((Author author) => author.FullName == authorName)) == null)
+                                    {
+                                        theAuthor = new Author
+                                        {
+                                            FullName = authorName
+                                        };
+                                    }
+                                    theBook = new Book
+                                    {
+                                        Title = bookTitile
+                                    };
+                                    theAuthor.Books.Add(theBook);
+                                    theBook.Authors.Add(theAuthor);
+                                }
+                                Reference item = new Reference
+                                {
+                                    Description = this.txtDescription.Text,
+                                    PageNumber = this.txtPageNo.Text,
+                                    Sentence = this.sentence,
+                                    Book = theBook
+                                };
+                                this.sentence.References.Add(item);
+                                keywordManagementContext.SaveChanges();
+                                break;
+                            }
+                    }
+                }
+            });
+            base.Close();
+        }
+
+        private void btnCancle_Click(object sender, EventArgs e)
+        {
+            base.Close();
+        }
+    }
+}
